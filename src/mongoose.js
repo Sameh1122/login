474728ae9d43b3e4f2953575d49c997d3b7f2bba
sameh kamal <sameh.engineer2016@gmail.com>
2020-03-01 10:27:02 +0200

    Setup app for Heroku

--- src/mongoose.js
+++ src/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require ('mongoose')
 require('./roots')
-mongoose.connect('mongodb://127.0.0.1:27017/loginSystem', {
+mongoose.connect('mongodb:https//127.0.0.1:27017/loginSystem', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndexes:true  
